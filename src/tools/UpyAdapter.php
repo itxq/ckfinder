f5a618ef51a72628375ec186b48f8e60075a3827
IT小强xqitw.cn <360237521@qq.com>
2019-07-11 13:16:49 +0800

    提升在列取又拍云文件列表时性能 解决当前文件夹下文件比较多（比如超过30个）时列出列表非常慢的问题

--- src/tools/UpyAdapter.php
+++ src/tools/UpyAdapter.php
@@ -26,22 +26,22 @@ use Upyun\Upyun;
 class UpyAdapter extends AbstractAdapter
 {
     use NotSupportingVisibilityTrait;
-    
+
     /**
      * @var array - 配置信息
      */
     protected $config = [];
-    
+
     /**
      * @var Upyun
      */
     protected $client;
-    
+
     /**
      * @var string - 又拍云远程路径前缀
      */
     protected $pathPrefix = '/';
-    
+
     /**
      * 又拍云储存
      * UpyAdapter 构造函数.
@@ -50,19 +50,19 @@ class UpyAdapter extends AbstractAdapter
      */
     public function __construct($config)
     {
-        $this->config = $config;
+        $this->config     = $config;
         $this->pathPrefix = $this->getSubValue('root', $config, '');
-        $serviceName = $this->getSubValue('service', $config, '');
-        $operatorName = $this->getSubValue('operator', $config, '');
+        $serviceName      = $this->getSubValue('service', $config, '');
+        $operatorName     = $this->getSubValue('operator', $config, '');
         $operatorPassword = $this->getSubValue('password', $config, '');
-        $serviceConfig = new \Upyun\Config($serviceName, $operatorName, $operatorPassword);
-        $this->client = new Upyun($serviceConfig);
+        $serviceConfig    = new \Upyun\Config($serviceName, $operatorName, $operatorPassword);
+        $this->client     = new Upyun($serviceConfig);
     }
-    
+
     /**
      * 拉取文件信息
      * @param string $directory - 目录
-     * @param bool $recursive
+     * @param bool   $recursive
      * @return array
      * @throws \Exception
      */
@@ -72,16 +72,16 @@ class UpyAdapter extends AbstractAdapter
         if ($directory !== '') {
             $directory = rtrim($directory, '/') . '/';
         }
-        $read = $this->readContent($directory, 'dir');
+        $read     = $this->readContent($directory, 'dir');
         $contents = [];
         if (is_array($read) && count($read) >= 1) {
             foreach ($read as $k => $v) {
-                $contents[] = $this->info($directory . $v['name']);
+                $contents[] = $this->getInfoByReadContent($v, $directory);
             }
         }
         return $recursive ? $contents : Util::emulateDirectories($contents);
     }
-    
+
     /**
      * 写入
      * @param string $path
@@ -93,19 +93,19 @@ class UpyAdapter extends AbstractAdapter
     {
         return $this->upload($path, $contents);
     }
-    
+
     /**
      * 写入
-     * @param string $path
+     * @param string   $path
      * @param resource $resource
-     * @param Config $config
+     * @param Config   $config
      * @return array|bool|false
      */
     public function writeStream($path, $resource, Config $config)
     {
         return $this->upload($path, $resource);
     }
-    
+
     /**
      * 更新
      * @param string $path
@@ -117,22 +117,22 @@ class UpyAdapter extends AbstractAdapter
     {
         return $this->upload($path, $contents);
     }
-    
+
     /**
      * 更新
-     * @param string $path
+     * @param string   $path
      * @param resource $resource
-     * @param Config $config
+     * @param Config   $config
      * @return array|bool|false
      */
     public function updateStream($path, $resource, Config $config)
     {
         return $this->upload($path, $resource);
     }
-    
+
     /**
      * 重命名
-     * @param string $path - 原始路径
+     * @param string $path    - 原始路径
      * @param string $newpath - 新路径
      * @return bool
      * @throws \Exception
@@ -145,7 +145,7 @@ class UpyAdapter extends AbstractAdapter
         }
         return false;
     }
-    
+
     /**
      * 复制
      * @param string $path
@@ -156,7 +156,7 @@ class UpyAdapter extends AbstractAdapter
     public function copy($path, $newpath): bool
     {
         $t_path = $this->applyPathPrefix($path);
-        $info = $this->info($t_path);
+        $info   = $this->info($t_path);
         if ($info['type'] === 'file') {
             try {
                 $up = $this->upload($newpath, $this->readContent($t_path, 'file'));
@@ -177,7 +177,7 @@ class UpyAdapter extends AbstractAdapter
         }
         return true;
     }
-    
+
     /**
      * 删除目录
      * @param string $dirname - 目录路径
@@ -188,7 +188,7 @@ class UpyAdapter extends AbstractAdapter
     {
         return $this->delete($dirname);
     }
-    
+
     /**
      * 创建目录
      * @param string $dirname - 目录路径
@@ -207,7 +207,7 @@ class UpyAdapter extends AbstractAdapter
             return false;
         }
     }
-    
+
     /**
      * 删除操作
      * @param string $path - 远程路径
@@ -236,7 +236,7 @@ class UpyAdapter extends AbstractAdapter
             return false;
         }
     }
-    
+
     /**
      * 判断文件是否存在于又拍云存储
      * @param string $path
@@ -251,7 +251,7 @@ class UpyAdapter extends AbstractAdapter
             return false;
         }
     }
-    
+
     /**
      * 读取文件
      * @param string $path
@@ -259,12 +259,12 @@ class UpyAdapter extends AbstractAdapter
      */
     public function read($path): array
     {
-        $path = $this->applyPathPrefix($path);
-        $info = $this->info($path);
+        $path             = $this->applyPathPrefix($path);
+        $info             = $this->info($path);
         $info['contents'] = $this->readContent($path, 'file');
         return $info;
     }
-    
+
     /**
      * 读取文件
      * @param string $path
@@ -273,18 +273,18 @@ class UpyAdapter extends AbstractAdapter
      */
     public function readStream($path)
     {
-        $path = $this->applyPathPrefix($path);
-        $info = $this->info($path);
+        $path            = $this->applyPathPrefix($path);
+        $info            = $this->info($path);
         $info['content'] = $this->readContent($path, 'file');
-        $info['stream'] = $info['content'];
-        $tempName = tempnam('', 'tmp');
+        $info['stream']  = $info['content'];
+        $tempName        = tempnam('', 'tmp');
         if (!$tempName || !file_put_contents($tempName, $info['content'])) {
             return $info;
         }
         $info['stream'] = fopen($tempName, 'rb');
         return $info;
     }
-    
+
     /**
      * 获取云存储文件/目录的基本信息
      * @param string $path
@@ -295,7 +295,7 @@ class UpyAdapter extends AbstractAdapter
         $path = $this->applyPathPrefix($path);
         return $this->info($path);
     }
-    
+
     /**
      * 获取文件 size
      * @param string $path
@@ -305,7 +305,7 @@ class UpyAdapter extends AbstractAdapter
     {
         return $this->getMetadata($path);
     }
-    
+
     /**
      * 获取文件 mimetype
      * @param string $path
@@ -315,7 +315,7 @@ class UpyAdapter extends AbstractAdapter
     {
         return $this->getMetadata($path);
     }
-    
+
     /**
      * 获取文件 timestamp
      * @param string $path
@@ -325,12 +325,12 @@ class UpyAdapter extends AbstractAdapter
     {
         return $this->getMetadata($path);
     }
-    
+
     /**
      * 建立标准化输出数组
      * @param string $path
-     * @param int $timestamp
-     * @param mixed $content
+     * @param int    $timestamp
+     * @param mixed  $content
      * @return array
      */
     protected function normalize($path, $timestamp, $content = null): array
@@ -346,25 +346,25 @@ class UpyAdapter extends AbstractAdapter
         }
         return $data;
     }
-    
+
     /**
      * 读取云存储文件/目录内容
-     * @param string $path - 路径
-     * @param string $type - file|dir 为空时自动判断
+     * @param string   $path   - 路径
+     * @param string   $type   - file|dir 为空时自动判断
      * @param null|mixed 文件内容写入本地文件流。例如 `$saveHandler = fopen('/local/file', 'w')`。
-     * 当设置该参数时，将以文件流的方式，直接将又拍云中的文件写入本地的文件流，或其他可以写入的流
-     * @param array $params 可选参数，读取目录内容时，
-     * 需要设置三个参数：`X-List-Iter` 分页开始位置（第一页不需要设置），`X-List-Limit` 获取的文件数量（默认 100，最大10000），`X-List-Order` 结果以时间正序或者倒序
-     * @param int|null $start 分页开始位置（不需要设置）
-     * @param array $list 返回的数据集（不需要设置）
+     *                         当设置该参数时，将以文件流的方式，直接将又拍云中的文件写入本地的文件流，或其他可以写入的流
+     * @param array    $params 可选参数，读取目录内容时，
+     *                         需要设置三个参数：`X-List-Iter` 分页开始位置（第一页不需要设置），`X-List-Limit` 获取的文件数量（默认 100，最大10000），`X-List-Order` 结果以时间正序或者倒序
+     * @param int|null $start  分页开始位置（不需要设置）
+     * @param array    $list   返回的数据集（不需要设置）
      * @return bool|mixed
      */
     protected function readContent($path, $type = '', $saveHandler = null, $params = [], $start = null, $list = [])
     {
         try {
             $params['X-List-Limit'] = 10000;
-            $params['X-List-Iter'] = $start;
-            $allowType = ['file', 'dir'];
+            $params['X-List-Iter']  = $start;
+            $allowType              = ['file', 'dir'];
             if (empty($type) || !in_array($type, $allowType, true)) {
                 $info = $this->info($path);
                 $type = $this->getSubValue('type', $info, false);
@@ -373,10 +373,10 @@ class UpyAdapter extends AbstractAdapter
                 return $this->client->read($path, $saveHandler, $params);
             }
             if ($type === 'dir') {
-                $data = $this->client->read($path, $saveHandler, $params);
-                $isEnd = (bool)$this->getSubValue('is_end', $data, true);
+                $data    = $this->client->read($path, $saveHandler, $params);
+                $isEnd   = (bool)$this->getSubValue('is_end', $data, true);
                 $curList = $this->getSubValue('files', $data, []);
-                $list = array_merge($list, $curList);
+                $list    = array_merge($list, $curList);
                 if ($isEnd === true) {
                     return $list;
                 }
@@ -388,13 +388,13 @@ class UpyAdapter extends AbstractAdapter
             return $list;
         }
     }
-    
+
     /**
      * 获取云存储文件/目录的基本信息
      * @param string $path 云存储的文件路径
      * @return array 返回标准化的数组
      */
-    protected function info($path): array
+    protected function info(string $path): array
     {
         $info = $this->client->info($path);
         $type = str_replace('*', '', $info['x-upyun-file-type']);
@@ -411,14 +411,40 @@ class UpyAdapter extends AbstractAdapter
             'type'      => 'file',
         ];
     }
-    
+
+    /**
+     * 根据读取的文件、目录内容，返回标准化数组
+     * @param array  $item
+     * @param string $directory
+     * @return array
+     */
+    protected function getInfoByReadContent(array $item, string $directory): array
+    {
+        $path      = $directory . $item['name'];
+        $noPrePath = $this->removePathPrefix($path);
+        if ($item['type'] !== 'N') {
+            return [
+                'type' => 'dir',
+                'path' => $this->removePathPrefix(rtrim($path, '/'))
+            ];
+        }
+        return [
+            'path'      => $noPrePath,
+            'timestamp' => (int)$item['time'],
+            'dirname'   => Util::dirname($noPrePath),
+            'mimetype'  => Util::guessMimeType($path, null),
+            'size'      => $item['size'],
+            'type'      => 'file',
+        ];
+    }
+
     /**
      * 文件上传云存储
-     * @param string $path Path
+     * @param string          $path     Path
      * @param string|resource $contents Either a string or a stream.
      * @return array|bool
      */
-    protected function upload($path, $contents)
+    protected function upload(string $path, $contents)
     {
         try {
             $path = $this->applyPathPrefix($path);
@@ -428,12 +454,12 @@ class UpyAdapter extends AbstractAdapter
             return false;
         }
     }
-    
+
     /**
      * 获取数组、对象下标对应值，不存在时返回指定的默认值
-     * @param string|integer $name - 下标（键名）
-     * @param array|object $data - 原始数组/对象
-     * @param mixed $default - 指定默认值
+     * @param string|integer $name    - 下标（键名）
+     * @param array|object   $data    - 原始数组/对象
+     * @param mixed          $default - 指定默认值
      * @return mixed
      */
     protected function getSubValue(string $name, $data, $default = '')
